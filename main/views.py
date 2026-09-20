8fbaebd27daee785fba462e14344254ba352a244
Idrism <idriskamal1107@gmail.com>
2026-09-20 21:25:47 +0700

    Add create, edit, and delete views for experience

--- main/views.py
+++ main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 from main.models import Experience,Education,Project
-from main.forms import EducationForm,ProjectForm
+from main.forms import EducationForm,ProjectForm,ExperienceForm
 
 from django.contrib import messages
 from django.core import serializers
@@ -53,6 +53,17 @@ def create_project(request):
         "form": form,
     }
     return render(request, "project_form.html", context)
+
+def create_experience(request):
+    form = ExperienceForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        messages.success(request, "Experience added!")
+        return redirect("main:show_experience")
+
+    context = {"name": "Idris", "form": form}
+    return render(request, "experience_form.html", context)
 
 def get_projects_json(request):
     title_query = request.GET.get("title", "").strip()
@@ -130,6 +141,16 @@ def delete_education(request, education_id):
 
     return redirect("main:show_education")
 
+def delete_experience(request, experience_id):
+    experience = get_object_or_404(Experience, pk=experience_id)
+
+    if request.method == "POST":
+        experience.delete()
+        messages.success(request, "Experience deleted!")
+        return redirect("main:show_experience")
+
+    return redirect("main:show_experience")
+
 def edit_education(request, education_id):
     education = get_object_or_404(Education, pk=education_id)
     form = EducationForm(request.POST or None, instance=education)
@@ -161,3 +182,15 @@ def edit_project(request, project_id):
         "project": project,
     }
     return render(request, "project_edit_form.html", context)
+
+def edit_experience(request, experience_id):
+    experience = get_object_or_404(Experience, pk=experience_id)
+    form = ExperienceForm(request.POST or None, instance=experience)
+
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        messages.success(request, "Experience updated!")
+        return redirect("main:show_experience")
+
+    context = {"name": "Idris", "form": form, "experience": experience}
+    return render(request, "experience_edit_form.html", context)
